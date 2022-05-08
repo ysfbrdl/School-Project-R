e3de508a3ce42a37c99bc6f5e66d974353642ea5
Yusuf-Cm <102414967+Yusuf-Cm@users.noreply.github.com>
2022-05-08 17:12:17 +0300

    School project training in Java

---
+++ src/School.java
@@ -0,0 +1,9 @@
+public interface School {
+    public static final String SCHOOL_NAME = "Techproed";
+
+    public abstract String departmentName(); //Current department name
+
+    public abstract Instructor instructor();
+
+    public abstract Student student();
+}
